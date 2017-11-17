0fbc5aace92a55e4e8ac806cfcff8a743e1483c4
markonick <nicolas.markos@gmail.com>
2017-11-17 11:57:57 +0000

    added bash startup script for docker

---
+++ installdocker.sh
@@ -0,0 +1,15 @@
+#--------------INSTALL DOCKER -------------------------------------------
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+
+sudo apt-get install -y docker-ce
+
+#----------------INSTALL DOCKER COMPOSE ---------------------------------------------
+curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+chmod +x /tmp/docker-machine &&
+
+#------------------SETUP TEST AND REBOOT--------------------------------
+sudo usermod -aG docker $USER
+sudo service docker restart
